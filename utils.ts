94c0fd46cb31fab0b11684799a4a7dd6ffa62c7c
Alex <24763887+QWxleA@users.noreply.github.com>
2022-03-31 00:38:02 +0200

    refactored, works, kind off

--- utils.ts
+++ utils.ts
@@ -1,92 +1,122 @@
 import '@logseq/libs'
+import { PageEntity, BlockEntity, SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin';
 export var blocks2 = []
 export function getBlocksInPage() {
-    logseq.Editor.getCurrentPageBlocksTree().then((block) => {
-        console.log(block)
-        for (const x in block) {
-            parseBlocksTree(block[x]);
+    // logseq.Editor.getCurrentPageBlocksTree().then((block) => {
+    //     console.log(block)
+    //     for (const x in block) {
+    //         parseBlocksTree(block[x]);
+    //     }
+
+    // })
+    // createExport();
+    createExport2()
+}
+
+async function parsePage(finalString:string, docTree) {
+    // console.log("aq3",docTree)
+    for (const x in docTree) { 
+        // console.log("x",x, docTree[x])
+        // skip meta-data
+        if ( ! (parseInt(x) === 0 && docTree[x].level === 1) ) {
+            // console.log("aq5",docTree[x].content)
+            finalString = `${finalString}\n${docTree[x].content}`;  
+            if (docTree[x].children.length > 0) finalString = await parsePage(finalString, docTree[x].children)
+            // console.log("aq5.5",finalString)      
         }
-
-    })
-    createExport();
-}
-
-async function createExport() {
-    var finalString = `# ${(await logseq.Editor.getCurrentPage()).originalName}`;
-    // var finalString = ``;
-
-    for (const x in blocks2) {
-        var formattedText = await formatText(blocks2[x][0], x);
-        finalString = `${finalString}\n${formattedText}`;
+        console.log("x",x)
     }
-
-    finalString = finalString.replaceAll("#+BEGIN_QUOTE", "");
-    finalString = finalString.replaceAll("#+END_QUOTE", "");
-    console.log(finalString)
-    download(`${(await logseq.Editor.getCurrentPage()).originalName}.orig`, finalString);
+    console.log("aq6",finalString)
+    return finalString
 }
 
-function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
+async function createExport2() {
+    let curPage = await logseq.Editor.getCurrentPage()
+    console.log("aq-o", curPage)
+    let finalString = `---\ntitle: \"${curPage.originalName}\"`
+    
+    const docTree = await logseq.Editor.getCurrentPageBlocksTree()
+    console.log("aq-1", docTree)
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
+    //page meta-data
+    for (const prop in curPage.properties) {
+        const pvalue = curPage.properties[prop]
+        finalString = `${finalString}\n${prop}::::`
+        //FIXME ugly
+        if ( Array.isArray(pvalue) ) { for (const key in pvalue) finalString=`${finalString}\n- ${pvalue[key]}`
+        } else { finalString=`${finalString}\n- ${pvalue}` }
+    } 
+    finalString = `${finalString}\n---`
 
-    element.click();
+    // parse page-content
+    finalString = await parsePage(finalString, docTree)
+    console.log("aq-2",finalString)
 
-    document.body.removeChild(element);
+    // finalString = finalString.replaceAll("#+BEGIN_QUOTE", "");
+    // finalString = finalString.replaceAll("#+END_QUOTE", "");
+    download(`${curPage.originalName}.md`, finalString);
 }
 
-
-export function parseBlocksTree(obj) {
-    conductParsing(obj);
-    function conductParsing(obj) {
-        if (obj.content) {
-            let content2 = obj.content
-            let level = obj.level;
-            blocks2.push([content2, level]);
+export async function parseMeta(metaLine) {
+    console.log("parseMata:",metaLine)
+    let count = 0
+    //every second match should be inserted as "\n - (match). Else, it should insert (match:) "
+    metaLine = metaLine.replaceAll(/((?<=::).*|.*::)/g, (match) => {
+        count++;
+        if (count % 2 == 0) {
+            //if the value  of "match", split with , is greater than 1, then for each value, insert  "\n - (match) else simply insert match 
+            if (match.split(",").length > 1) {
+                let finalString = "";
+                match.split(",").map(x => {
+                    finalString = `${finalString}\n - ${x}`;
+                })
+                console.log(finalString)
+                return finalString.replaceAll("::", ":");
+            }
+            else {
+                console.log("PPP1 match?",match)
+                return ` ${match}`;
+            }
+        } else {
+            console.log("PPP2 match?",match.replaceAll("::", ":"))
+            return match.replaceAll("::", ":");
         }
-        obj.children.map(conductParsing);
-    }
+    });
 }
-
 
 
 export async function formatText(text2, number) {
+    console.log("formatText:", text2, number)
     var text: string = text2.replace(/:LOGBOOK:|collapsed:: true/gi, "");
     if (text.includes("CLOCK: [")) {
         text = text.substring(0, text.indexOf("CLOCK: ["));
     }
 
-    // if (logseq.settings[`${template}Options`].includes("Hide Page Properties")) {
-    if (number == 0) {
-        let count = 0
-        //every second match should be inserted as "\n - (match). Else, it should insert (match:) "
-        text = text.replaceAll(/((?<=::).*|.*::)/g, (match) => {
-            count++;
-            if (count % 2 == 0) {
-                //if the value  of "match", split with , is greater than 1, then for each value, insert  "\n - (match) else simply insert match 
-                if (match.split(",").length > 1) {
-                    let finalString = "";
-                    match.split(",").map(x => {
-                        finalString = `${finalString}\n - ${x}`;
-                    })
-                    console.log(finalString)
-                    return finalString.replaceAll("::", ":");
-                }
-                else {
-                    return ` ${match}`;
-                }
-            } else {
-                console.log(match)
-                return match.replaceAll("::", ":");
-            }
-        });
-        /// Add --- above and below the text
-        text = `---\n${text}\n---`;
-    }
+    // // if (logseq.settings[`${template}Options`].includes("Hide Page Properties")) {
+    // if (number == 0) {
+    //     let count = 0
+    //     //every second match should be inserted as "\n - (match). Else, it should insert (match:) "
+    //     text = text.replaceAll(/((?<=::).*|.*::)/g, (match) => {
+    //         count++;
+    //         if (count % 2 == 0) {
+    //             //if the value  of "match", split with , is greater than 1, then for each value, insert  "\n - (match) else simply insert match 
+    //             if (match.split(",").length > 1) {
+    //                 let finalString = "";
+    //                 match.split(",").map(x => {
+    //                     finalString = `${finalString}\n - ${x}`;
+    //                 })
+    //                 console.log(finalString)
+    //                 return finalString.replaceAll("::", ":");
+    //             }
+    //             else {
+    //                 return ` ${match}`;
+    //             }
+    //         } else {
+    //             console.log("match?",match)
+    //             return match.replaceAll("::", ":");
+    //         }
+    //     });
+    // }
 
     //conversion of links to hugo syntax
     if (logseq.settings.linkFormat == "Hugo Format") {
@@ -127,8 +157,39 @@ export async function formatText(text2, number) {
     }
 }
 
+//FIXME don't get it, but it works
+function download(filename, text) {
+    // console.log("DB:download", filename, text)
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
   //Conversions to be handled
-  //1. Convert page properties
+  //1. DONE Convert page properties 
   //2. Remove bullets and indentation
   //3. Translate links from [[Logseq export]] should be translated as `[Logseq export]({{< ref "Logseq_export" >}})`
   //4. Convert to .orig file
+
+  //OLD
+
+export function parseBlocksTree(obj) {
+    conductParsing(obj);
+    function conductParsing(obj) {
+        if (obj.content) {
+            let content2 = obj.content
+            let level = obj.level;
+            blocks2.push([content2, level]);
+        }
+        obj.children.map(conductParsing);
+    }
+}
+
+
+async function createExport() {}

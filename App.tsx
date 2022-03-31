d84273ab822830fb34b2c4cdeb7364eeb3019d00
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-03-31 18:28:35 +0400

    initialize react

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, { useRef, useState } from "react";
+import ReactDOM from "react-dom";
+
+const App: React.FC = ({ blockID }) => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default App

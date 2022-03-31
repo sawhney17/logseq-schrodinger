24f01612105010b19fd5e0b3ef96aaf14e46c9d8
Alex <24763887+QWxleA@users.noreply.github.com>
2022-03-31 03:09:49 +0200

    refactor cleaned up

--- utils.ts
+++ utils.ts
@@ -1,33 +1,9 @@
 import '@logseq/libs'
 import { PageEntity, BlockEntity, SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin';
 export var blocks2 = []
-export function getBlocksInPage() {
-    // logseq.Editor.getCurrentPageBlocksTree().then((block) => {
-    //     console.log(block)
-    //     for (const x in block) {
-    //         parseBlocksTree(block[x]);
-    //     }
 
-    // })
-    // createExport();
-    createExport2()
-}
-
-async function parsePage(finalString:string, docTree) {
-    for (const x in docTree) { 
-        // skip meta-data
-        if ( ! (parseInt(x) === 0 && docTree[x].level === 1) ) {
-            // console.log("aq5",docTree[x].content)
-            finalString = `${finalString}\n${await parseText(docTree[x])}`;  
-            if (docTree[x].children.length > 0) finalString = await parsePage(finalString, docTree[x].children)
-            // console.log("aq5.5",finalString)      
-        }
-    }
-    console.log("aq6",finalString)
-    return finalString
-}
-
-async function createExport2() {
+export async function getBlocksInPage() {
+// async function createExport2() {
     let txt:str = ""
     const curPage = await logseq.Editor.getCurrentPage()
     const docTree = await logseq.Editor.getCurrentPageBlocksTree()
@@ -55,43 +31,35 @@ async function createExport2() {
     download(`${curPage.originalName}.md`, finalString);
 }
 
-    // export async function parseMeta(metaLine) {
-    //     console.log("parseMata:",metaLine)
-    //     let count = 0
-    //     //every second match should be inserted as "\n - (match). Else, it should insert (match:) "
-    //     metaLine = metaLine.replaceAll(/((?<=::).*|.*::)/g, (match) => {
-    //         count++;
-    //         if (count % 2 == 0) {
-    //             //if the value  of "match", split with , is greater than 1, then for each value, insert  "\n - (match) else simply insert match 
-    //             if (match.split(",").length > 1) {
-    //                 let finalString = "";
-    //                 match.split(",").map(x => {
-    //                     finalString = `${finalString}\n - ${x}`;
-    //                 })
-    //                 console.log(finalString)
-    //                 return finalString.replaceAll("::", ":");
-    //             }
-    //             else {
-    //                 console.log("PPP1 match?",match)
-    //                 return ` ${match}`;
-    //             }
-    //         } else {
-    //             console.log("PPP2 match?",match.replaceAll("::", ":"))
-    //             return match.replaceAll("::", ":");
-    //         }
-    //     });
-    // }
+
+async function parsePage(finalString:string, docTree) {
+    for (const x in docTree) { 
+        // skip meta-data
+        if ( ! (parseInt(x) === 0 && docTree[x].level === 1) ) {
+            // console.log("aq5",docTree[x].content)
+            finalString = `${finalString}\n${await parseText(docTree[x])}`;  
+            if (docTree[x].children.length > 0) finalString = await parsePage(finalString, docTree[x].children)
+            // console.log("aq5.5",finalString)      
+        }
+    }
+    console.log("aq6",finalString)
+    return finalString
+}
 
 
 async function parseText(block:BlockEntity) {
     let text = block.content
-    console.log("txt",text)
-    let txtBefore:string = "\n"
+    console.log("block",block)
+    let txtBefore:string = ""
     let txtAfter:string  = ""
+    const prevBlock:BlockEntity = await logseq.Editor.getBlock(block.left.id, {includeChildren: false})
+    console.log("prevBlock",prevBlock)
 
     //add ?
-    // if ( block.level === 1) txtAfter = "\n"
-    if ( block.level > 1 ) txtBefore = " ".repeat((block.level -1) * 2) + "+"
+    if ( block.level > 1 ) {
+        txtBefore = " ".repeat((block.level -1) * 2) + "+"
+        txtBefore = "\n" + txtBefore
+    }
     //exceptions
     if ( text.substring(0,3) === "```" ) txtBefore = ""
     //indent text + add newline after block
@@ -115,56 +83,13 @@ async function parseText(block:BlockEntity) {
     return text
 } 
 
-//convert
+//import into parseText
 export async function formatText(text2, number) {
     // console.log("formatText:", text2, number)
     var text: string = text2.replace(/:LOGBOOK:|collapsed:: true/gi, "");
     if (text.includes("CLOCK: [")) {
         text = text.substring(0, text.indexOf("CLOCK: ["));
     }
-
-    // // if (logseq.settings[`${template}Options`].includes("Hide Page Properties")) {
-    // if (number == 0) {
-    //     let count = 0
-    //     //every second match should be inserted as "\n - (match). Else, it should insert (match:) "
-    //     text = text.replaceAll(/((?<=::).*|.*::)/g, (match) => {
-    //         count++;
-    //         if (count % 2 == 0) {
-    //             //if the value  of "match", split with , is greater than 1, then for each value, insert  "\n - (match) else simply insert match 
-    //             if (match.split(",").length > 1) {
-    //                 let finalString = "";
-    //                 match.split(",").map(x => {
-    //                     finalString = `${finalString}\n - ${x}`;
-    //                 })
-    //                 console.log(finalString)
-    //                 return finalString.replaceAll("::", ":");
-    //             }
-    //             else {
-    //                 return ` ${match}`;
-    //             }
-    //         } else {
-    //             console.log("match?",match)
-    //             return match.replaceAll("::", ":");
-    //         }
-    //     });
-    // }
-
-    // //conversion of links to hugo syntax
-    // if (logseq.settings.linkFormat == "Hugo Format") {
-    //     text = text.replaceAll(/\[\[.*\]\]/g, (match) => {
-    //         return `[${match.substring(2, match.length - 2)}]({{${match.substring(2, match.length - 2).replaceAll(" ", "_")}}}`
-    //     })
-    // }
-    // if  (logseq.settings.linkFormat == "Without brackets") {
-    //     text = text.replaceAll("[[", "")
-    //     text = text.replaceAll("]]", "")
-    // }
-
-    // //convert code blocks and replace every second match with a newline at start
-    // let count = 0;
-    // text = text.replaceAll(/(?<!`)`(?!`)/g, (match) => { count += 1; return count % 2 == 0 ? "\n```" : "```\n" })
-
-
 
     const rxGetId = /\(\(([^)]*)\)\)/;
     const blockId = rxGetId.exec(text);
@@ -204,23 +129,6 @@ function download(filename, text) {
 }
   //Conversions to be handled
   //1. DONE Convert page properties 
-  //2. Remove bullets and indentation
-  //3. Translate links from [[Logseq export]] should be translated as `[Logseq export]({{< ref "Logseq_export" >}})`
+  //2. Remove bullets and indentation ???? in progress
+  //3. DONE Translate links from [[Logseq export]] should be translated as `[Logseq export]({{< ref "Logseq_export" >}})`
   //4. Convert to .orig file
-
-  //OLD
-
-export function parseBlocksTree(obj) {
-    conductParsing(obj);
-    function conductParsing(obj) {
-        if (obj.content) {
-            let content2 = obj.content
-            let level = obj.level;
-            blocks2.push([content2, level]);
-        }
-        obj.children.map(conductParsing);
-    }
-}
-
-
-async function createExport() {}

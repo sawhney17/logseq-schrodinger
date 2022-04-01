3fc6cbb0ce1f0218eede84ff471771e778fa2731
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-04-01 15:47:21 +0400

    WIP: single export UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,17 @@ const App: React.FC = () => {
       <div className="overlay">
       <div className="flex justify-center w-screen">
           <div className="smartblock-inserter">
-              <label>HI</label>
-              <label>Bye</label>
-              <label>You are cool</label>
-              <label>You are mean</label>
+            <h1 className=" w-max text-left text-xl">Hugo Export</h1>
+            <div className="grid grid-cols-2 gap-4 place-items-auto">
+              <p>Note Name</p>
+              <input id="Note Name"type="text" />
+              <p>Hugo File Name</p>
+              <input id="Hugo File Name"type="text" />
+              <p>Original Date</p>
+              <input id="Original Date"type="text" />
+              <p>Updated Post Date</p>
+              <input id="Updated Post Date"type="text" />
+            </div>
             </div>
       </div>
       </div>

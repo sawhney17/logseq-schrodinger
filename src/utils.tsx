eda17ecdb6875441e14e4180596c522aaed0b953
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-04-16 22:33:51 +0400

    fix timestamps

--- src/utils.tsx
+++ src/utils.tsx
@@ -54,7 +54,24 @@ export async function getAllPublicPages() {
 
 function hugoDate(timestamp) {
   let date = new Date(timestamp);
-  return `${date.getFullYear()}-${("0" + (date.getMonth()+1))}-${(date.getDate())}`
+
+  //if date.getdate does not have a zero, add A ZERO BEFORE IT
+  let month;
+  if (date.getMonth() + 1 < 10){
+    month = `0${date.getMonth() + 1}`
+  }
+  else{
+    month = `${date.getMonth() + 1}`
+  }
+  let day;
+  if (date.getDate() < 10){
+    day = `0${date.getDate()}`
+  }
+  else{
+    day = `${date.getDate()}`
+  }
+  
+  return `${date.getFullYear()}-${month}-${day}`
 }
 
 //parse files meta-data

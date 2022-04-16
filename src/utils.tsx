2bf483aaabfbf0bb5243b6c3e16be8cdaf9d1a66
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-04-16 22:58:09 +0400

    fix emoji in filename

--- src/utils.tsx
+++ src/utils.tsx
@@ -191,7 +191,7 @@ export async function getBlocksInPage(
     download(`${titleDetails[1].hugoFileName}.md`, finalString);
   } else {
     console.log(e["original-name"])
-    zip.file(`${curPage["original-name"]}.md`, finalString);
+    zip.file(`${curPage["original-name"].replaceAll(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')}.md`, finalString);
 
     if (isLast) {
       setTimeout(() => {
